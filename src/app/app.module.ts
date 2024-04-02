1f218c279ea8007bd68e3a1501d48969d7931630
Catalina Osorio <catalinaandrade026@gmail.com>
2024-04-02 11:02:23 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardProductComponent,
+    HeaderComponent,
+    CatalogoComponent,
+    AboutComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+
+  
+})
+export class AppModule { }
+
---
+++ src/app/components/card-product/card-product.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-card-product',
+  templateUrl: './card-product.component.html',
+  styleUrls: ['./card-product.component.scss'],
+})
+export class CardProductComponent implements OnInit {
+  @Input() product = {
+    name: '',
+    price: 0,
+    description: '',
+    inventory: 0,
+    image: '',
+    cantidad: 0,
+  };
+
+  @Output() addToCart = new EventEmitter();
+
+  cantidad: number = 0;
+  soldOut: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  addProduct() {
+    if (this.cantidad < this.product.inventory) {
+      this.soldOut = false;
+      this.cantidad++;
+    } else {
+      this.soldOut = true;
+    }
+  }
+
+  removeProduct() {
+    if (this.cantidad > 0) {
+      this.soldOut = false;
+      this.cantidad--;
+    }
+  }
+
+  addToCartHandler() {
+    if (this.cantidad > 0) {
+      this.product['cantidad'] = this.cantidad;
+      this.addToCart.emit(this.product);
+      this.cantidad = 0;
+    }
+  }
+}
